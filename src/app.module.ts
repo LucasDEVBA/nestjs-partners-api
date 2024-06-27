04217c1c9bd472399cf8b5db373ed5df383ce1d3
Lucas <lucasdevba@gmail.com>
2024-06-27 04:04:42 -0300

    chore: add prsma service and configuration connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
 
+@Global()
 @Module({
   imports: [EventsModule, PrismaModule],
   controllers: [AppController],
